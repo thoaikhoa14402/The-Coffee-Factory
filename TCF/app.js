3a6188a39a540cb6ad7e80cb3c5863cd927b307c
thoaikhoa14402 <nguyenthoaidangkhoa@gmail.com>
2022-06-07 23:43:46 +0700

    Revise product schema

--- TCF/app.js
+++ TCF/app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 //const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
 const cors = require('cors');
 const AppError = require('./utilities/appError');
 const globalErrorhandler = require('./controllers/errorController');
 
 const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+
+const mime = {
+  html: 'text/html',
+  txt: 'text/plain',
+  css: 'text/css',
+  gif: 'image/gif',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  js: 'application/javascript',
+};
+
+const dir = path.join(__dirname);
 
 const app = express();
 
@@ -14,6 +30,7 @@ app.use(cors()); // cap phep cho toan bo clients co the truy cap
 // mounting routers
 
 app.use('/tcf/v1/users', userRouter); // mounting new router on route (URL)
+app.use('/tcf/v1/products', productRouter);
 
 //app.get('/tcf/v1/users', (req,res) -)
 
@@ -31,7 +48,17 @@ app.all('*', (req, res, next) => {
   // err.statusCode = 404;
   // next(err);
   // method 3 (refactoring)
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  const file = path.join(dir, req.path);
+  const type = mime[path.extname(file).slice(1)];
+  const s = fs.createReadStream(file);
+  //console.log(req.path);
+  s.on('open', () => {
+    res.set('Content-Type', type);
+    s.pipe(res);
+  });
+  s.on('error', () => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  });
 });
 
 // error handling middleware
