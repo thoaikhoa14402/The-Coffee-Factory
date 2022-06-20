18e6e46f3ebf3a26f7bf4c3950fd5cb38f833cfd
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2022-06-20 18:19:28 +0700

    Delete TCF directory

--- TCF/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const productController = require('../controllers/productController');
-
-const router = express.Router();
-
-// product handle
-router.get('/get-products', productController.sendData);
-
-module.exports = router;
