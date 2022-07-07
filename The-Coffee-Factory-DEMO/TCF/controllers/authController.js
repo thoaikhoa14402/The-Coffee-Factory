43b3ad3973bae82c999ce4f9ada2ae59dfcedc01
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2022-07-07 18:48:37 +0700

    Update authController.js

--- The-Coffee-Factory-DEMO/TCF/controllers/authController.js
+++ The-Coffee-Factory-DEMO/TCF/controllers/authController.js
@@ -73,10 +73,11 @@ exports.login = catchAsync(async (req, res, next) => {
 // protect handle
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check of it's there
-  let token;
+  let token = '';
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
   }
+  if (token == '' && req.body.userAction == 'order') next();
   if (!token) {
     return next(new AppError('You are not logged in! Please log in to get access'), 401);
   }
