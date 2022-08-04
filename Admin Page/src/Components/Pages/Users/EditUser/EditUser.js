901d97bddfa20f6acac5ea9e2b8e9178e694f616
khangduy017 <78061687+khangduy017@users.noreply.github.com>
2022-08-04 10:59:08 +0700

    Add files via upload

---
+++ Admin Page/src/Components/Pages/Users/EditUser/EditUser.js
@@ -0,0 +1,151 @@
+import React, { useState, useEffect } from 'react'
+import "./EditUser.css"
+import axios from 'axios'
+import Cookie from 'universal-cookie'
+import io from 'socket.io-client'
+import back from "../../../../Images/double-left.png"
+import { useNavigate, useParams } from 'react-router-dom'
+
+const cookie = new Cookie()
+const socket = io.connect(process.env.REACT_APP_ipAddress)
+
+export default function AddUser() {
+    const params = useParams();
+    const [gender, setGender] = useState();
+    const [status, setStatus] = useState();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [role, setRole] = useState();
+    const [setSuc, setSaveEditSuccess] = useState(false)
+    const [dataEdit, setDataEdit] = useState('')
+
+    const navigate = useNavigate()
+
+    const sendDataUpdate = () => {
+        const headers = {
+            'Authorization': `Bearer ${cookie.get("JWT")}`
+        }
+
+        const object = {
+            name: name,
+            email: email,
+            gender: gender,
+            status: status,
+            role: role
+        }
+
+        axios.patch(process.env.REACT_APP_ipAddress + '/tcf/v1/admin/update-user/' + params.id, object, { headers: headers }).then((res) => {
+            console.log(name)
+            console.log(gender)
+            console.log(res.data)
+            setSaveEditSuccess(true)
+            setTimeout(() => {
+                setSaveEditSuccess(false)
+            }, 3000);
+
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+    useEffect(() => {
+        const headers = {
+            'Authorization': `Bearer ${cookie.get("JWT")}`
+        }
+        axios.get(process.env.REACT_APP_ipAddress + '/tcf/v1/admin/get-user-by-id/' + params.id, { headers: headers }).then((res) => {
+            setDataEdit(res.data.data)
+            setName(res.data.data.name)
+            setEmail(res.data.data.email)
+            setGender(res.data.data.gender)
+            setStatus(res.data.data.status)
+            setRole(res.data.data.role)
+            console.log(res.data.data)
+
+        }).catch(error => {
+            console.log(error)
+        })
+    }, [])
+
+
+    return (
+        <div className='AddUser'>
+            <div className='back-allUser' onClick={() => navigate('/users')}>
+                <svg className="btn-back" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0_1802_6518)">
+                        <path d="M9.11553 1.75272e-06L10.8843 1.83588L3.01865 10L10.8843 18.1641L9.11553 20L0.365526 10.9179C-0.121974 10.4119 -0.121974 9.59131 0.365526 9.08206L9.11553 1.75272e-06Z" fill="black" />
+                        <path d="M1.25 11.2974L20 11.2974L20 8.70255L1.25 8.70255L1.25 11.2974Z" fill="black" />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_1802_6518">
+                            <rect width="20" height="20" fill="white" transform="translate(20 20) rotate(-180)" />
+                        </clipPath>
+                    </defs>
+                </svg>
+
+                <p className='btn-name'>All User</p>
+            </div>
+
+            <div className='title-newUser'>
+                <p className='new-user-title'>Edit User</p>
+                <p className='describe-title'>Use the below form to edit a account</p>
+            </div>
+
+            <div className='form-user'>
+                <p className='title-name-form'>Name</p>
+                <input className='input-name' type='text' placeholder='Nguyen Van A' value={name}
+                    onChange={(event) => { setName(event.target.value) }}></input>
+
+                <p className='title-name-mail'>Email</p>
+                <input className='input-email' type='email' placeholder='example@gmail.com' value={email}
+                    onChange={(event) => { setEmail(event.target.value) }}></input>
+
+            </div>
+
+            <div className='gender-user'>
+                <p className='gender-title'>Gender</p>
+                <div className='male-gender'>
+                    <input type='radio' name='gender' checked={(gender === 'male') ? true : false} onClick={() => { setGender('male') }} value='Male' />Male
+                </div>
+
+                <div className='female-gender'>
+                    <input type='radio' name='gender' checked={(gender === 'female') ? true : false} onClick={() => { setGender('female') }} value='Female' />Female
+                </div>
+                <div className='other-gender'>
+                    <input type='radio' name='gender' checked={(gender === 'other') ? true : false} onClick={() => { setGender('other') }} value='Other' />Other
+                </div>
+            </div>
+
+            <div className='status-user'>
+                <p className='status-title'>Status</p>
+                <div className='status1'>
+                    <input type='radio' name='status' checked={(status === 'active') ? true : false} onClick={() => { setStatus('active') }} value='Active' />Active
+                </div>
+
+                <div className='status2'>
+                    <input type='radio' name='status' checked={(status === 'inactive') ? true : false} onClick={() => setStatus('inactive')} value='Inactive' />Inactive
+                </div>
+            </div>
+
+            <div className='role-user'>
+                <p className='role-title'>Role</p>
+                <div className='role-1'>
+                    <input type='radio' name='role' checked={(role === 'admin') ? true : false} onClick={() => { setRole('admin') }} value='Admin' />Admin
+                </div>
+
+                <div className='role-2'>
+                    <input type='radio' name='role' checked={(role === 'employee') ? true : false} onClick={() => { setRole('employee') }} value='Employee' />Employee
+                </div>
+
+                <div className='role-3'>
+                    <input type='radio' name='role' checked={(role === 'customer') ? true : false} onClick={() => { setRole('customer') }} value='Customer' />Customer
+                </div>
+            </div>
+
+            {!setSuc && <div className='save-user-btn' onClick={() => { sendDataUpdate() }}>
+                <p className='save-user'>save</p></div>}
+            {setSuc && <div className='save-user-btn save-user-btn-bg' onClick={() => { sendDataUpdate() }}>
+                <p className='save-user'>save successfully</p></div>}
+
+        </div>
+    )
+}
---
+++ Admin Page/src/Components/Pages/Users/Users.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from "react-router-dom";
+import './Users.css'
+import ItemUsers from './ItemUsers/ItemUsers'
+import axios from 'axios'
+import Cookie from 'universal-cookie'
+import io from 'socket.io-client'
+import product from '../../../Images/product.png'
+import customer from '../../../Images/customer.png'
+import employee from '../../../Images/employee.png'
+import adduser from '../../../Images/adduser.png'
+import search from '../../../Images/search.png'
+
+const cookie = new Cookie()
+const socket = io.connect(process.env.REACT_APP_ipAddress)
+
+export default function Users() {
+    const [dataU, setData] = useState('')
+    useEffect(() => {
+        const headers = {
+            'Authorization': `Bearer ${cookie.get("JWT")}`
+        }
+        axios.get(process.env.REACT_APP_ipAddress + '/tcf/v1/admin/get-all-users', { headers: headers }).then((res) => {
+            setData(res.data.data.users)
+            console.log(res.data.data.users)
+        }).catch(error => {
+            console.log(error)
+        })
+    }, [])
+
+    const navigate = useNavigate()
+
+    const sendStage = (object) => {
+        const headers = {
+            'Authorization': `Bearer ${cookie.get("JWT")}`
+        }
+        axios.post(process.env.REACT_APP_ipAddress + '/tcf/v1/admin/get-all-users', object, { headers: headers }).then((res) => {
+            setData((prevData) => prevData.map((element) => (element._id === res.data.data.users._id) ? res.data.data.users : element))
+
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+    const remove = (id) => {
+        setData(dataU.filter((el) => el._id !== id))
+    }
+
+    useEffect(() => {
+        socket.on("users", (data) => {
+            setData((prevData) => [...prevData, data])
+        });
+    }, [])
+
+    return (
+        <div className='Users'>
+            <div className='UserTitle'>Users Management</div>
+            <div className='UserInformation'>
+                <div className='ui ui-1'>
+                    <p className='uiTitle'>Customers</p>
+                    <p className='uiAmount'>5</p>
+                    <div className='uiIcon'>
+                        <img src={customer} alt='' />
+                    </div>
+                </div>
+                <div className='ui ui-2'>
+                    <p className='uiTitle'>Admin</p>
+                    <p className='uiAmount'>3</p>
+                    <div className='uiIcon'>
+                        <img src={product} alt='' />
+                    </div>
+                </div>
+                <div className='ui ui-3'>
+                    <p className='uiTitle'>Employees</p>
+                    <p className='uiAmount'>2</p>
+                    <div className='uiIcon'>
+                        <img src={employee} alt='' />
+
+                    </div>
+                </div>
+            </div>
+
+            <div className='search-form'>
+                <img src={search} alt='' />
+                <input className='input-search' type='text' placeholder='Search...'></input>
+            </div>
+
+            <div onClick={() => navigate('/users/addUser')} className='add-user'>
+                <p className='new-user'>New User</p>
+                <div className='au-icon'>
+                    <img src={adduser} alt='' />
+                </div>
+            </div>
+
+            <div className='recentUsers'>
+                <div className='recentUsersTitle'>
+                    <p>Recent Users</p>
+                </div>
+                <div className='recentUserName'>
+                    <p className='id'>ID</p>
+                    <p className='name-user'>Name</p>
+                    <p className='email'>Email</p>
+                    <p className='gender'>Gender</p>
+                    <p className='status'>Status</p>
+                    <p className='actionC'>Action</p>
+                </div>
+                <div className='mainUser'>
+                    {dataU.length > 0 && dataU.map((values, index) => (
+                        <ItemUsers key={index}
+                            id={values._id}
+                            nameUser={values.name}
+                            emailUser={values.email}
+                            gender={values.gender}
+                            statusUser={values.status}
+                            callback={sendStage}
+                            remove={remove}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
